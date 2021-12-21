99612fe431256baa053fe1fb64ffcd98ef210c15
Daniel Perez Bartolome <96476985+danielperezbart@users.noreply.github.com>
2021-12-21 13:15:02 +0100

    Update Result.kt
    prueba

--- app/src/main/java/com/example/myapplication/data/Result.kt
+++ app/src/main/java/com/example/myapplication/data/Result.kt
@@ -10,6 +10,7 @@ sealed class Result<out T : Any> {
     data class Error(val exception: Exception) : Result<Nothing>()
 
     override fun toString(): String {
+        // prueba
         return when (this) {
             is Success<*> -> "Success[data=$data]"
             is Error -> "Error[exception=$exception]"
